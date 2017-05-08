c183387f6cec018bb389795fa5a09995a1468f3f
Cedric Nicoloso <Cedric_Nicoloso@amp.com.au>
2017-05-08 21:49:22 +1000

    Add a navigation left menu

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,49 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">CropChat</span>
+      </div>
+    </header>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">CropChat</span>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="/#/" @click="hideMenu">Home</a>
+        <a class="mdl-navigation__link" href="/#/post" @click="hideMenu">Post a picture</a>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  require('material-design-lite')
+
+  export default {
+    name: 'app',
+    methods: {
+      hideMenu: function () {
+        document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+        document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
